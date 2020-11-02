7e118a2c8687c2f133ec1862362227a0d6e7abea
ytkachev <spacestar.ru@gmail.com>
2020-11-03 01:56:19 +0300

    add login/registration scenario

---
+++ app/src/main/java/com/handcontrol/ui/start/login/LoginFragment.kt
@@ -0,0 +1,33 @@
+package com.handcontrol.ui.start.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.handcontrol.R
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loginButton.setOnClickListener {
+            activity?.finish()
+            it.findNavController().navigate(R.id.action_global_navigation)
+        }
+        registrationButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_registrationFragment)
+        )
+    }
+}
