4f2dfa059a812475007ca8b97d7da118f43fb568
nikaalin <lina.y2018@ya.ru>
2020-11-14 17:56:59 +0300

    Added BaseFragment.kt

---
+++ app/src/main/java/com/handcontrol/base/BaseFragment.kt
@@ -0,0 +1,40 @@
+package com.handcontrol.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.databinding.library.baseAdapters.BR
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+abstract class BaseFragment<DB : ViewDataBinding, VM : ViewModel>(
+    private val viewModelClass: Class<VM>,
+    private val layoutRes: Int
+) : Fragment() {
+
+    lateinit var binding: DB
+    protected lateinit var viewModel: VM
+    open var viewModelFactory: ViewModelProvider.Factory? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = if (viewModelFactory == null)
+            ViewModelProvider(this).get(viewModelClass)
+        else ViewModelProvider(this, viewModelFactory!!).get(viewModelClass)
+        binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        binding.let {
+            it.setVariable(BR.viewModel, viewModel)
+            it.lifecycleOwner = viewLifecycleOwner
+            it.executePendingBindings()
+        }
+        return binding.root
+    }
+}
--- app/src/main/java/com/handcontrol/ui/main/gestures/GesturesFragment.kt
+++ app/src/main/java/com/handcontrol/ui/main/gestures/GesturesFragment.kt
@@ -1,40 +1,29 @@
 package com.handcontrol.ui.main.gestures
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.handcontrol.R
+import com.handcontrol.base.BaseFragment
 import com.handcontrol.base.BaseRecyclerAdapter
+import com.handcontrol.databinding.FragmentGesturesBinding
 import com.handcontrol.model.Gesture
 import kotlinx.android.synthetic.main.fragment_gestures.*
 
-class GesturesFragment : Fragment() {
+class GesturesFragment : BaseFragment<FragmentGesturesBinding, GesturesViewModel>(
+    GesturesViewModel::class.java,
+    R.layout.fragment_gestures
+) {
 
-    private lateinit var gesturesViewModel: GesturesViewModel
 //    private val PERMISSIONS_RECORD_AUDIO = 200
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        gesturesViewModel =
-            ViewModelProvider(this).get(GesturesViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_gestures, container, false)
-
-        return root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(gestureRecycler) {
             adapter = BaseRecyclerAdapter<Gesture, GestureListener>(
                 R.layout.list_item_gesture,
-                gesturesViewModel.listData.value!!,
+                viewModel.listData.value!!,
                 object : GestureListener {
                     override fun onClick(item: Gesture) {
                         //TODO("Not yet implemented")
@@ -52,6 +41,7 @@ class GesturesFragment : Fragment() {
             }
         }
     }
+
 
 //  Вставьте фрагмент кода, когда пользователь захочет использовать голосовые команды
 //    if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
