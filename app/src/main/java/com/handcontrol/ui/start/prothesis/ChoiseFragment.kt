ed284f692f147d94a99c9d116e2de2ef2438c8eb
VladimirRozov <39130781+VladimirRozov@users.noreply.github.com>
2020-12-06 19:23:28 +0300

    gprc auto grpc new client list of prothesis search protesis

--- app/src/main/java/com/handcontrol/ui/start/prothesis/ChoiseFragment.kt
+++ app/src/main/java/com/handcontrol/ui/start/prothesis/ChoiseFragment.kt
@@ -2,49 +2,83 @@ package com.handcontrol.ui.start.prothesis
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView.OnItemClickListener
-import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.github.mikephil.charting.charts.Chart.LOG_TAG
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
 import com.handcontrol.R
+import com.handcontrol.adapter.AdapterClass
+import com.handcontrol.api.Api
+import com.handcontrol.repository.SearchQuery
 import com.handcontrol.ui.main.Navigation
-import kotlinx.android.synthetic.main.fragment_choise.*
-import kotlinx.android.synthetic.main.list_item.*
+import io.grpc.StatusRuntimeException
+import kotlinx.coroutines.launch
+import java.util.logging.Logger
 
 
 class ChoiseFragment : Fragment() {
-    val devices = arrayOf(
-        "Proto_1", "Proto_2", "Proto_3",
+    lateinit var list: ListView
+    var adapter: AdapterClass? = null
+    var editsearch: SearchView? = null
+    var searchQueries: Array<String> = arrayOf(
+           "Proto_1", "Proto_2", "Proto_3",
     )
-    private var mAdapter: ArrayAdapter<String>? = null
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
 
-    ): View? {
+    var arraylist = ArrayList<SearchQuery>()
+    override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+
+): View? {
         val rootView: View = inflater.inflate(R.layout.fragment_choise, container, false)
         (activity as AppCompatActivity?)!!.supportActionBar!!.show()
-        mAdapter = ArrayAdapter<String>(requireContext(), R.layout.list_item, devices)
-        val listView: ListView = rootView.findViewById(R.id.list) as ListView
-        listView.setAdapter(mAdapter)
+        Api.clearProthesis()
 
+        lifecycleScope.launch {
+        try{
+            var proto = Api.getGrpcHandler().getProto()
+            proto = proto.replace("[", "")
+            proto = proto.replace("]", "")
+            val parts = proto.split(", ")
+                // Logger.getLogger(ChoiseFragment::class.java.name).warning(proto)
+            for (searchQuery in parts) {
+                val searchQuery1 = SearchQuery(searchQuery)
+                arraylist.add(searchQuery1)
+            }
+        }catch (e: StatusRuntimeException){
+            e.printStackTrace()
+        }
+        }
+
+        list = rootView.findViewById(R.id.list) as ListView
+        adapter = AdapterClass(this, arraylist)
+        list.adapter = adapter
+        editsearch = rootView.findViewById(R.id.search) as SearchView
+        editsearch!!.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                adapter!!.filter(newText)
+                return false
+            }
+        })
 
         return rootView
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        list.setOnItemClickListener(OnItemClickListener { parent, view, position, id ->
+        list.setOnItemClickListener(OnItemClickListener { _, _, _, id ->
             val intent = Intent(context, Navigation::class.java)
+            Api.saveProthesis(id.toString())
             startActivity(intent)
         })
-
-
     }
 }
--- app/src/main/java/com/handcontrol/ui/start/registration/RegistrationFragment.kt
+++ app/src/main/java/com/handcontrol/ui/start/registration/RegistrationFragment.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.handcontrol.R
+import com.handcontrol.api.Api
+import com.handcontrol.api.HandlingType
+import io.grpc.StatusRuntimeException
 import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_login.registrationButton
+import kotlinx.android.synthetic.main.fragment_registration.*
+import kotlinx.coroutines.launch
 
 class RegistrationFragment : Fragment() {
 
@@ -22,9 +32,31 @@ class RegistrationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        registrationButton.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_registrationFragment_to_choiseFragment)
-        )
+        val name: EditText = view.findViewById(R.id.name) as EditText
+        val login: EditText = view.findViewById(R.id.new_login) as EditText
+        val password: EditText = view.findViewById(R.id.new_password) as EditText
+        registrationButton.setOnClickListener {
+            Api.setHandlingType(HandlingType.GRPC)
+            if (!name.text.isBlank() &&!login.text.isBlank() && !password.text.isBlank()) {
+                lifecycleScope.launch {
+                    try {
+                        Snackbar.make(it, "wait...", Snackbar.LENGTH_INDEFINITE).show()
+                        Api.getGrpcHandler()
+                            .registration(name.text.toString(), login.text.toString(), password.text.toString())
+                        Snackbar.make(it, "Registrated", Snackbar.LENGTH_SHORT).show()
+                        if (Api.isRegistrated()) {
+                            findNavController().navigate(R.id.action_registrationFragment_to_choiseFragment)
+                        } else {
+                            Snackbar.make(it, "Not registrated", Snackbar.LENGTH_SHORT).show()
+                        }
+                    } catch (e: StatusRuntimeException) {
+                        e.printStackTrace()
+                        Snackbar.make(it, "error", Snackbar.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Snackbar.make(it, "Name, login or password is empty", Snackbar.LENGTH_SHORT).show()
+            }
+        }
     }
 }
